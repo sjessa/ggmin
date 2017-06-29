1799c86bc9a2e2804e30660bec7e87cc6ee11bd6
selinj <selinjessa@gmail.com>
2017-06-29 16:56:42 -0400

    :tada: Add a clean theme for ggplot2

---
+++ R/theme_min.R
@@ -0,0 +1,27 @@
+
+#' @name theme_min
+#' @title A clean theme for ggplot2
+#'
+#' @export
+theme_min <- function(base_size = 11, base_family = "") {
+
+  theme_light(base_size = 11, base_family = "") +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      panel.border = element_rect(fill = NA, colour = "black", size = rel(1)),
+      strip.background = element_rect(fill = NA, colour = NA),
+      strip.text.x = element_text(colour = "black"),
+      strip.text.y = element_text(colour = "black"),
+      title = element_text(size = rel(0.9)),
+      axis.text = element_text(colour = "black", size = rel(0.8)),
+      axis.title = element_text(colour = "black", size = rel(0.9)),
+      legend.title = element_text(colour = "black", size = rel(0.9)),
+      legend.key.size = unit(0.9, "lines"),
+      legend.text = element_text(size = rel(0.7), colour = "black"),
+      legend.key = element_rect(colour = NA, fill = NA),
+      legend.background = element_rect(colour = NA, fill = NA)
+    )
+}
+
