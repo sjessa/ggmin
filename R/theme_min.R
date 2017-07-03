95a07866faa24283b9a1bcee47df60724ad5172b
selinj <selinjessa@gmail.com>
2017-07-03 10:03:04 -0400

    Lighten border, add example

--- R/theme_min.R
+++ R/theme_min.R
@@ -4,16 +4,16 @@
 #'
 #' @export
 theme_min <- function(base_size = 11, base_family = "") {
-
+  
   theme_light(base_size = 11, base_family = "") +
     theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
       panel.background = element_blank(),
-      panel.border = element_rect(fill = NA, colour = "black", size = rel(1)),
+      panel.border = element_rect(fill = NA, colour = "grey90", size = 1),
       strip.background = element_rect(fill = NA, colour = NA),
-      strip.text.x = element_text(colour = "black"),
-      strip.text.y = element_text(colour = "black"),
+      strip.text.x = element_text(colour = "black", size = rel(1.2)),
+      strip.text.y = element_text(colour = "black", size = rel(1.2)),
       title = element_text(size = rel(0.9)),
       axis.text = element_text(colour = "black", size = rel(0.8)),
       axis.title = element_text(colour = "black", size = rel(0.9)),
@@ -24,4 +24,5 @@ theme_min <- function(base_size = 11, base_family = "") {
       legend.background = element_rect(colour = NA, fill = NA)
     )
 }
+
 
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
+---
+output:
+  pdf_document: default
+  html_document: default
+---
+
 # ggmin
 A clean, minimalist theme for ggplot2
+
 
 # install
 ```
@@ -8,11 +15,27 @@ devtools::install_github("selinj/ggmin")
 ```
 
 # use
-
 ```
+library(ggmin)
+
 df %>%
   ggplot(aes(x = X, y = y)) +
   ... + # Other layers (geoms, etc.) here
   ggmin::theme_min()
 
 ```
+
+# example
+```
+library(ggmin)
+
+mpg %>% 
+  filter(class %in% c("compact", "suv", "pickup", "minivan")) %>% 
+  ggplot(aes(x = displ, y = hwy)) +
+  geom_point(aes(colour = factor(year))) +
+  facet_wrap(~ class) +
+  ggtitle("A scatterplot") +
+  ggmin::theme_min()
+  
+```
+![](fig/mpg.pdf)
