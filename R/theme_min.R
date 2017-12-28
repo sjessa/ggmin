a3edfe2431bb0fe7bac1c9cff00e698548782eee
sjessa <selinjessa@gmail.com>
2017-12-27 20:48:42 -0800

    Add a new theme with nice defaults for Powerpoints

--- R/theme_min.R
+++ R/theme_min.R
@@ -4,7 +4,7 @@
 #'
 #' @export
 theme_min <- function(base_size = 11, base_family = "") {
-  
+
   theme_light(base_size = 11, base_family = "") +
     theme(
       panel.grid.major = element_blank(),
@@ -26,3 +26,33 @@ theme_min <- function(base_size = 11, base_family = "") {
 }
 
 
+#' @name theme_min_ppt
+#' @title A clean theme for ggplot2, with some handy defaults for Powerpoints
+#'
+#' Essentially \code{\link{theme_min}} with larger text in certain elements,
+#' and a little room between axis titles and the plot.
+#'
+#' @export
+theme_min_ppt <- function(base_size = 11, base_family = "") {
+
+  theme_light(base_size = 11, base_family = "") +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      panel.border = element_rect(fill = NA, colour = "grey90", size = 1),
+      strip.background = element_rect(fill = NA, colour = NA),
+      strip.text.x = element_text(colour = "black", size = rel(1.2)),
+      strip.text.y = element_text(colour = "black", size = rel(1.2)),
+      title = element_text(size = rel(0.9)),
+      axis.text = element_text(colour = "black", size = rel(1.1)),
+      axis.title = element_text(colour = "black", size = rel(1.2)),
+      axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
+      axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+      legend.title = element_text(colour = "black", size = rel(1.1)),
+      legend.key.size = unit(0.9, "lines"),
+      legend.text = element_text(size = rel(1.0), colour = "black"),
+      legend.key = element_rect(colour = NA, fill = NA),
+      legend.background = element_rect(colour = NA, fill = NA)
+    )
+}
